4d124662bb75f6e9cd7cb3483cefa23757650c15
ShivendraPandey26 <aviralpandey7974@gmail.com>
2024-03-22 01:37:09 +0530

    Create TypeData.jsx

---
+++ src/Components/TypeData.jsx
@@ -0,0 +1,14 @@
+import React, { useRef, useState } from "react";
+import Data from "./Data";
+
+function TypeData() {
+
+
+  return (
+    <>
+      <Data></Data>
+    </>
+  );
+}
+
+export default TypeData;
